f54050a00ea28469faa2b1e85cfeebfb342da1d2
Connor Heeney <s2570912@ed.ac.uk>
2024-02-11 16:09:58 +0000

    Adding pubs data.

---
+++ pubs.sql
@@ -0,0 +1,200 @@
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.878884 57.1589389) 
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.8051993 57.1748318)
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8335374 57.1826621) 
+    )
+)/
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.8304327 57.1838681) 
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8298885 57.1855039) 
+    )
+)/
+
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8298054 57.1856435) 
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.8338257 57.1882943) 
+    )
+)/
+
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.829134199999999 57.1880985) 
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.8285842 57.19619609999999) 
+    )
+)/
+
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8295966 57.1912289) 
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8284774 57.1928083) 
+    )
+)/
+
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.8173007 57.1996776)
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8243827 57.2011138)
+    )
+)/
+
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.8243454 57.2012904)
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.8341948 57.1878309)
+    )
+)/
+
+
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1),
+        SDO_ORDINATE_ARRAY(-3.6704068 57.1339311)
+    )
+)/
+
+INSERT INTO PUBS (GEOM)
+VALUES (
+    SDO_GEOMETRY(
+        2001, 
+        NULL,
+        NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1, 1), 
+        SDO_ORDINATE_ARRAY(-3.3986582 57.0060009)
+    )
+)/
+
